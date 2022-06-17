c9706260522114bf46bb3b7e950a8d57986e0132
Roman Zarytskyi <afy@ukr.net>
2022-06-17 23:52:10 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
                     © {new Date().getFullYear()} Copyright Text
                     <a
                         className='grey-text text-lighten-4 right'
-                        href='https://github.com/michey85/react-food'
+                        href='https://github.com/zarik93/food'
                         rel='noreferrer'
                         target='_blank'
                     >
